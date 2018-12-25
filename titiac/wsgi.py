45928eacaaf7da72ab1525d828875f61236b9324
Administrator <Administrator@USER-20170809TJ>
2018-12-25 21:03:31 +0800

    init

---
+++ titiac/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for titiac project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'titiac.settings')
+
+application = get_wsgi_application()
