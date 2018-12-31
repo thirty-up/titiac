6aad8b8027eeb7e73a107bb6360c0eb3f0ffe346
Administrator <Administrator@windows10.microdone.cn>
2018-12-31 18:29:09 +0800

    add server to global

--- server/udp/boot.py
+++ server/udp/boot.py
@@ -2,8 +2,7 @@
 """
 @since: 2018-12-28
 @version: 0.0.1
-@author: LIUJG
-@contact: 575616873@QQ
+@author: 1949.LIU
 """
 
 from server.udp.udpsocket import Server
@@ -16,6 +15,7 @@ def start():
         init a instance of the udp server
     """
     
+    global server 
     server = Server(_UDP_PORT)
     server.listen(receive, _UDP_BUFFER_SIZE)
     
--- server/udp/udpsocket.py
+++ server/udp/udpsocket.py
@@ -2,8 +2,7 @@
 """
 @since: 2018-12-28
 @version: 0.0.1
-@author: LIUJG
-@contact: 575616873@QQ
+@author: 1949.LIU
 """
 
 import socket
